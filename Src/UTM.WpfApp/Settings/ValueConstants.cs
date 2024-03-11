ae7a9b0dc362d137b21ce07f486c70faf5e8b08c
usama <29387130+usa-m@users.noreply.github.com>
2024-03-11 14:44:30 +0500

    removing constants for el and fc

--- Src/UTM.WpfApp/Settings/ValueConstants.cs
+++ Src/UTM.WpfApp/Settings/ValueConstants.cs
@@ -7,8 +7,5 @@ internal static class ValueConstants
     public readonly static double ExperimentTimeMinimumSec = 5;
     public readonly static double ExperimentTimeMaximumSec = 500;
 
-    public readonly static double FuelCellCurrentMeasurementResistanceOhm = 1;
-    public readonly static double ElectrolyzerCurrentMeasurementResistanceOhm = 1;
-
     public readonly static string DefaultPasswordHash = "admin".Hash();
 }
