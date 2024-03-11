4c8873450611a2f6b7a548e0b712f608d7ba2edc
usama <29387130+usa-m@users.noreply.github.com>
2024-03-11 14:44:16 +0500

    removing the el and fc values

--- Src/UTM.WpfApp/InternalServices/DataExchangeService.cs
+++ Src/UTM.WpfApp/InternalServices/DataExchangeService.cs
@@ -1,24 +1,10 @@
-﻿using CronBlocks.UTM.Settings;
-using CronBlocks.Helpers;
+﻿using CronBlocks.Helpers;
 
 namespace CronBlocks.UTM.InternalServices;
 
 public class DataExchangeService : IDisposable
 {
     private readonly IniConfigIO _iniConfig;
-
-    /// <summary>
-    /// Experiment duration in seconds
-    /// </summary>
-    public double ExperimentTimeDuration { get; set; }
-    /// <summary>
-    /// Resistance value in Ohm (Ω)
-    /// </summary>
-    public double FuelCellCurrentMeasurementResistance { get; set; }
-    /// <summary>
-    /// Resistance value in Ohm (Ω)
-    /// </summary>
-    public double ElectrolyzerCurrentMeasurementResistance { get; set; }
 
     public DataExchangeService(IniConfigIO iniConfig)
     {
@@ -29,18 +15,12 @@ public class DataExchangeService : IDisposable
 
     private void LoadConfig()
     {
-        ExperimentTimeDuration = _iniConfig.GetDouble(KeyOf(nameof(ExperimentTimeDuration)), ValueConstants.ExperimentTimeMinimumSec + 20);
-
-        FuelCellCurrentMeasurementResistance = _iniConfig.GetDouble(KeyOf(nameof(FuelCellCurrentMeasurementResistance)), ValueConstants.FuelCellCurrentMeasurementResistanceOhm);
-        ElectrolyzerCurrentMeasurementResistance = _iniConfig.GetDouble(KeyOf(nameof(ElectrolyzerCurrentMeasurementResistance)), ValueConstants.ElectrolyzerCurrentMeasurementResistanceOhm);
+        //- TODO Read from INI file
     }
 
     private void SaveConfig()
     {
-        _iniConfig.SetDouble(KeyOf(nameof(ExperimentTimeDuration)), ExperimentTimeDuration);
-
-        _iniConfig.SetDouble(KeyOf(nameof(FuelCellCurrentMeasurementResistance)), FuelCellCurrentMeasurementResistance);
-        _iniConfig.SetDouble(KeyOf(nameof(ElectrolyzerCurrentMeasurementResistance)), ElectrolyzerCurrentMeasurementResistance);
+        //- TODO Store values into INI file
 
         _iniConfig.SaveFile();
     }
