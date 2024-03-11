34569267bb1e6674420342ea39480d1df8c80e2a
usama <29387130+usa-m@users.noreply.github.com>
2024-03-11 16:51:17 +0500

    adding calibration service iface

---
+++ Src/CronBlocks.SerialPortInterface/Interfaces/ISerialModbusDataCalibrationService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Immutable;
+
+namespace CronBlocks.SerialPortInterface.Interfaces;
+
+public interface ISerialModbusDataCalibrationService : IDisposable
+{
+    event Action<List<double>>? NewValuesReceived;
+    
+    void LoadValuesFromFile();
+    void SaveValuesToFile();
+    
+    void SetInputRange(double rangeMin, double rangeMax);
+    ImmutableDictionary<double,double> GetCalibrationValues();
+    ImmutableDictionary<double,double> SetCalibrationValues(double input, double output);
+}
