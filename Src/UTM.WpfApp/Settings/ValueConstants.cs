db248ae570b000ab3cadcc401b2cece93b51d579
usama <29387130+usa-m@users.noreply.github.com>
2024-03-11 14:46:09 +0500

    no min and max experiment times

--- Src/UTM.WpfApp/Settings/ValueConstants.cs
+++ Src/UTM.WpfApp/Settings/ValueConstants.cs
@@ -4,8 +4,5 @@ namespace CronBlocks.UTM.Settings;
 
 internal static class ValueConstants
 {
-    public readonly static double ExperimentTimeMinimumSec = 5;
-    public readonly static double ExperimentTimeMaximumSec = 500;
-
     public readonly static string DefaultPasswordHash = "admin".Hash();
 }
