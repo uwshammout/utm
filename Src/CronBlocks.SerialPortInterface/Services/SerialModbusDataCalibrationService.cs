f6309066ef07269ce5853317b956cf83dca99c6d
usama <29387130+usa-m@users.noreply.github.com>
2024-03-13 21:14:16 +0500

    formatting

--- Src/CronBlocks.SerialPortInterface/Services/SerialModbusDataCalibrationService.cs
+++ Src/CronBlocks.SerialPortInterface/Services/SerialModbusDataCalibrationService.cs
@@ -8,112 +8,115 @@ namespace CronBlocks.SerialPortInterface.Services;
 
 public class SerialModbusDataCalibrationService : ISerialModbusDataCalibrationService
 {
-    public event Action<List<double>>? NewValuesReceived;
+	public event Action<List<double>>? NewValuesReceived;
 
-    private readonly ISerialModbusDataScalingService _scalingService;
-    private readonly ILogger<SerialModbusDataCalibrationService> _logger;
-    private readonly string _filename;
-    private readonly IniConfigIO _iniConfig;
+	private readonly ISerialModbusDataScalingService _scalingService;
+	private readonly ILogger<SerialModbusDataCalibrationService> _logger;
+	private readonly string _filename;
+	private readonly IniConfigIO _iniConfig;
 
-    private double[] _calibratedValues;
-    private List<Dictionary<double,double>> _calibrationData;
+	private double[] _calibratedValues;
+	private List<Dictionary<double, double>> _calibrationData;
 
-    public SerialModbusDataCalibrationService(
-        ISerialModbusDataScalingService scalingService,
-        ILogger<SerialModbusDataCalibrationService> logger,
-        string filename = null!,
-        ILogger<IniConfigIO> iniLogger = null!)
-    {
-        _scalingService = scalingService;
-        _logger = logger;
-        _filename = filename;
-        _iniConfig = null!;
+	public SerialModbusDataCalibrationService(
+			ISerialModbusDataScalingService scalingService,
+			ILogger<SerialModbusDataCalibrationService> logger,
+			string filename = null!,
+			ILogger<IniConfigIO> iniLogger = null!)
+	{
+		_scalingService = scalingService;
+		_logger = logger;
+		_filename = filename;
+		_iniConfig = null!;
 
-        _calibratedValues = new double[Constants.TotalRegisters];
-        _calibrationData = new();
+		_calibratedValues = new double[Constants.TotalRegisters];
+		_calibrationData = new();
 
-        for (int i = 0; i < Constants.TotalRegisters; i++)
-        {
-            _calibrationData.Add([]);
-        }
+		for (int i = 0; i < Constants.TotalRegisters; i++)
+		{
+			_calibrationData.Add([]);
+		}
 
-        if (string.IsNullOrEmpty(_filename))
-        {
-            _logger.LogInformation($"No filename is provided, so we are using defaults of no calibration.");
-        }
-        else
-        {
-            _iniConfig = new(_filename, iniLogger);
-            LoadCalibrationDataFromFile();
-        }
+		if (string.IsNullOrEmpty(_filename))
+		{
+			_logger.LogInformation($"No filename is provided, so we are using defaults of no calibration.");
+		}
+		else
+		{
+			_iniConfig = new(_filename, iniLogger);
+			LoadCalibrationDataFromFile();
+		}
 
-        _scalingService.NewValuesReceived += OnNewScaledValuesReceived;
-    }
+		_scalingService.NewValuesReceived += OnNewScaledValuesReceived;
+	}
 
-    public void LoadCalibrationDataFromFile()
-    {
-        if (_iniConfig != null)
-        {
-            // TODO: Load calibration data
-        }
-    }
+	public void LoadCalibrationDataFromFile()
+	{
+		if (_iniConfig != null)
+		{
+			// TODO: Load calibration data
+		}
+	}
 
-    public void SaveCalibrationDataToFile()
-    {
-        if (_iniConfig != null)
-        {
-            // TODO: Save calibration data
+	public void SaveCalibrationDataToFile()
+	{
+		if (_iniConfig != null)
+		{
+			foreach (Dictionary<double, double> portCalibrationData in _calibrationData)
+			{
 
-            _iniConfig.SaveFile();
-        }
-    }
+			}
 
-    public void Dispose()
-    {
-        try
-        {
-            _scalingService.NewValuesReceived -= OnNewScaledValuesReceived;
-        }
-        catch { }
+			_iniConfig.SaveFile();
+		}
+	}
 
-        SaveCalibrationDataToFile();
-    }
+	public void Dispose()
+	{
+		try
+		{
+			_scalingService.NewValuesReceived -= OnNewScaledValuesReceived;
+		}
+		catch { }
 
-    private void OnNewScaledValuesReceived(List<double> scaledValues)
-    {
-        if (scaledValues == null)
-        {
-            _logger.LogError($"The list of 'values' received from scaling service is null.");
-            return;
-        }
-        else if (scaledValues.Count < Constants.TotalRegisters)
-        {
-            _logger.LogError(
-                $"The list of 'values' received from scaling service has {scaledValues.Count} values," +
-                $" whereas {Constants.TotalRegisters} values are expected.");
-            return;
-        }
+		SaveCalibrationDataToFile();
+	}
 
-        for (int i = 0; i < Constants.TotalRegisters; i++)
-        {
-            _calibratedValues[i] = ApplyCalibration(i, scaledValues[i]);
-        }
+	private void OnNewScaledValuesReceived(List<double> scaledValues)
+	{
+		if (scaledValues == null)
+		{
+			_logger.LogError($"The list of 'values' received from scaling service is null.");
+			return;
+		}
+		else if (scaledValues.Count < Constants.TotalRegisters)
+		{
+			_logger.LogError(
+					$"The list of 'values' received from scaling service has {scaledValues.Count} values," +
+					$" whereas {Constants.TotalRegisters} values are expected.");
+			return;
+		}
 
-        NewValuesReceived?.Invoke(_calibratedValues.ToList());
-    }
+		for (int i = 0; i < Constants.TotalRegisters; i++)
+		{
+			_calibratedValues[i] = ApplyCalibration(i, scaledValues[i]);
+		}
 
-    private double ApplyCalibration(int portIndex, double scaledValue)
-    {
-        throw new NotImplementedException();
-    }
+		NewValuesReceived?.Invoke(_calibratedValues.ToList());
+	}
 
-    public ImmutableDictionary<double, double> GetCalibrationValues(int portIndex)
-    {
-        throw new NotImplementedException();
-    }
+	private double ApplyCalibration(int portIndex, double scaledValue)
+	{
+		throw new NotImplementedException();
+	}
 
-    public ImmutableDictionary<double, double> SetCalibrationValues(int portIndex, ImmutableDictionary<double, double> calibrationPoints)
-    {
-        throw new NotImplementedException();
-    }
+	public ImmutableDictionary<double, double> GetCalibrationValues(int portIndex)
+	{
+		throw new NotImplementedException();
+	}
+
+	public ImmutableDictionary<double, double> SetCalibrationValues(int portIndex, ImmutableDictionary<double, double> calibrationPoints)
+	{
+		throw new NotImplementedException();
+	}
 }
