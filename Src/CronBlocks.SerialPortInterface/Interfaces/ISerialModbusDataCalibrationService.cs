cb55f982c091fda60737cf5d70bb742d0976debd
usama <29387130+usa-m@users.noreply.github.com>
2024-03-13 21:14:48 +0500

    formatting update

--- Src/CronBlocks.SerialPortInterface/Interfaces/ISerialModbusDataCalibrationService.cs
+++ Src/CronBlocks.SerialPortInterface/Interfaces/ISerialModbusDataCalibrationService.cs
@@ -4,11 +4,11 @@ namespace CronBlocks.SerialPortInterface.Interfaces;
 
 public interface ISerialModbusDataCalibrationService : IDisposable
 {
-    event Action<List<double>>? NewValuesReceived;
-    
-    void LoadCalibrationDataFromFile();
-    void SaveCalibrationDataToFile();
-    
-    ImmutableDictionary<double,double> GetCalibrationValues(int portIndex);
-    ImmutableDictionary<double,double> SetCalibrationValues(int portIndex, ImmutableDictionary<double, double> calibrationPoints);
+	event Action<List<double>>? NewValuesReceived;
+
+	void LoadCalibrationDataFromFile();
+	void SaveCalibrationDataToFile();
+
+	ImmutableDictionary<double, double> GetCalibrationValues(int portIndex);
+	ImmutableDictionary<double, double> SetCalibrationValues(int portIndex, ImmutableDictionary<double, double> calibrationPoints);
 }
