8fe6d324165bc5b8dcb8226eff2cf87a011f5738
usama <29387130+usa-m@users.noreply.github.com>
2024-03-13 23:40:46 +0500

    adding user control for digital value display

---
+++ Src/UTM.WpfApp/UserControls/DigitalValueDisplay.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CronBlocks.UTM.UserControls
+{
+	public partial class DigitalValueDisplay : UserControl
+	{
+		public DigitalValueDisplay()
+		{
+			InitializeComponent();
+		}
+	}
+}
