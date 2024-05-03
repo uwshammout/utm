3820abae212d0a539db3b64153ca8c800b93e9c4
usama <29387130+usa-m@users.noreply.github.com>
2024-05-03 13:40:24 +0500

    chaining calls for services setup

--- Src/UTM.WpfApp/Startup/ConfigureServices.cs
+++ Src/UTM.WpfApp/Startup/ConfigureServices.cs
@@ -13,60 +13,59 @@ internal static class ConfigureServices
 {
     public static void ConfigureAppServices(this IServiceCollection services, App app)
     {
-        services.AddSingleton(_ => app);
-        services.AddSingleton(_ => services);
-
-        //- Serial Port Services
-        services.AddSingleton<ISerialPortsDiscoveryService, SerialPortsDiscoveryService>();
-        services.AddSingleton<ISerialModbusClientService>(sp =>
-        {
-            return new SerialModbusClientService(
-                sp.GetRequiredService<ILogger<SerialModbusClientService>>(),
-                new IniConfigIO(
-                    FilePaths.ModbusSvcFilename,
-                    sp.GetRequiredService<ILogger<IniConfigIO>>()));
-        });
-        services.AddSingleton<ISerialOptionsService, SerialOptionsService>();
-        services.AddSingleton<ISerialModbusDataScalingService>(sp =>
-        {
-            return new SerialModbusDataScalingService(
-                sp.GetRequiredService<ILogger<SerialModbusDataScalingService>>(),
-                sp.GetRequiredService<ISerialModbusClientService>(),
-                FilePaths.DataScalingFilename,
-                sp.GetRequiredService<ILogger<IniConfigIO>>());
-        });
-        services.AddSingleton<ISerialModbusDataCalibrationService>(sp =>
-        {
-            return new SerialModbusDataCalibrationService(
-                sp.GetRequiredService<ISerialModbusDataScalingService>(),
-                sp.GetRequiredService<ILogger<SerialModbusDataCalibrationService>>(),
-                FilePaths.DataCalibrationFilename,
-                sp.GetRequiredService<ILogger<IniConfigIO>>());
-        });
-        services.AddSingleton(sp =>
-        {
-            return new DataExchangeService(
-                new IniConfigIO(
-                    FilePaths.DataExchangeSvcFilename,
-                    sp.GetRequiredService<ILogger<IniConfigIO>>()));
-        });
-
-        //- Windows
-        services.AddSingleton<MainWindow>();
-        services.AddTransient<DeviceConnectionWindow>();
-        services.AddTransient(sp =>
-        {
-            return new DeviceCalibrationWindow(
-                sp.GetRequiredService<ISerialModbusClientService>(),
-                sp.GetRequiredService<ISerialModbusDataScalingService>(),
-                sp.GetRequiredService<ISerialModbusDataCalibrationService>(),
-                sp.GetRequiredService<DataExchangeService>(),
-                new IniConfigIO(
-                    FilePaths.PasswordFilename,
-                    sp.GetRequiredService<ILogger<IniConfigIO>>())
-                );
-        });
-        services.AddTransient<MeasurementSettingsWindow>();
-        services.AddTransient<AboutWindow>();
+        services
+            .AddSingleton(_ => app)
+            .AddSingleton(_ => services)
+            //- Serial Port Services
+            .AddSingleton<ISerialPortsDiscoveryService, SerialPortsDiscoveryService>()
+            .AddSingleton<ISerialModbusClientService>(sp =>
+            {
+                return new SerialModbusClientService(
+                    sp.GetRequiredService<ILogger<SerialModbusClientService>>(),
+                    new IniConfigIO(
+                        FilePaths.ModbusSvcFilename,
+                        sp.GetRequiredService<ILogger<IniConfigIO>>()));
+            })
+            .AddSingleton<ISerialOptionsService, SerialOptionsService>()
+            .AddSingleton<ISerialModbusDataScalingService>(sp =>
+            {
+                return new SerialModbusDataScalingService(
+                    sp.GetRequiredService<ILogger<SerialModbusDataScalingService>>(),
+                    sp.GetRequiredService<ISerialModbusClientService>(),
+                    FilePaths.DataScalingFilename,
+                    sp.GetRequiredService<ILogger<IniConfigIO>>());
+            })
+            .AddSingleton<ISerialModbusDataCalibrationService>(sp =>
+            {
+                return new SerialModbusDataCalibrationService(
+                    sp.GetRequiredService<ISerialModbusDataScalingService>(),
+                    sp.GetRequiredService<ILogger<SerialModbusDataCalibrationService>>(),
+                    FilePaths.DataCalibrationFilename,
+                    sp.GetRequiredService<ILogger<IniConfigIO>>());
+            })
+            .AddSingleton(sp =>
+            {
+                return new DataExchangeService(
+                    new IniConfigIO(
+                        FilePaths.DataExchangeSvcFilename,
+                        sp.GetRequiredService<ILogger<IniConfigIO>>()));
+            })
+            //- Windows
+            .AddSingleton<MainWindow>()
+            .AddTransient<DeviceConnectionWindow>()
+            .AddTransient(sp =>
+            {
+                return new DeviceCalibrationWindow(
+                    sp.GetRequiredService<ISerialModbusClientService>(),
+                    sp.GetRequiredService<ISerialModbusDataScalingService>(),
+                    sp.GetRequiredService<ISerialModbusDataCalibrationService>(),
+                    sp.GetRequiredService<DataExchangeService>(),
+                    new IniConfigIO(
+                        FilePaths.PasswordFilename,
+                        sp.GetRequiredService<ILogger<IniConfigIO>>())
+                    );
+            })
+            .AddTransient<MeasurementSettingsWindow>()
+            .AddTransient<AboutWindow>();
     }
 }
